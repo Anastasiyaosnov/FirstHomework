f6bf78162267652133c36e202a11014142cad798
Anastasiya <anastasiyaosnov.mail.ru>
2022-12-08 19:50:41 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число, чтобы определить, является ли оно чётным");
+double userValue = Convert.ToDouble(Console.ReadLine());
+
+if (userValue % 2 == 0)
+{
+    Console.WriteLine($"{userValue} -> да");
+}
+else
+{
+    Console.WriteLine($"{userValue} -> нет");
+}
+
